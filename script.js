1af27a7c44faa6b95cce0a638c56fedbca592771
COSMO-source <Cosmo.ragnarok25@gmail.com>
2025-07-06 11:43:52 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// Simple scroll effect or animation logic can go here
+document.querySelector('.cta-button').addEventListener('click', () => {
+  document.querySelector('#explore').scrollIntoView({ behavior: 'smooth' });
+});
