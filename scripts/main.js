537e9ba1b7db4cee25f2a13b6e407dc461994de7
COSMO-source <Cosmo.ragnarok25@gmail.com>
2025-07-06 22:26:28 +1000

    Delete scripts directory

--- scripts/main.js
+++
@@ -1,5 +0,0 @@
-
-document.querySelector('.cta-button').addEventListener('click', (e) => {
-    e.preventDefault();
-    document.getElementById('explore').scrollIntoView({ behavior: 'smooth' });
-});
