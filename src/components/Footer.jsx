51191a05edf007604eb10055a3ffe4dd47fcce27
COSMO-source <Cosmo.ragnarok25@gmail.com>
2025-07-06 22:25:57 +1000

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <footer style={{ textAlign: 'center', marginTop: '50px' }}>
-      <p>&copy; 2025 UniVerse AI. All rights reserved.</p>
-    </footer>
-  )
-}
